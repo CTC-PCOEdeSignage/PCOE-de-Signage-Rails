4db0c4a917b6807d47aef360907644baaf1dce52
Ricky Chilcott <ricky@rakefire.io>
2018-06-02 17:42:03 -0400

    Dynamicize Slide Styles

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Slide < ApplicationRecord
   has_many :playlist_slides
   has_many :playlists, through: :playlist_slides
@@ -5,8 +7,11 @@ class Slide < ApplicationRecord
   has_one_attached :image
 
   def self.styles
-    %w(libcal-all-schedule-slide image markup)
-    # TODO: Add dynamic styles
+    ext = ".html.erb"
+    style_root = Rails.root.join("app", "views", "slides", "**", "*#{ext}").to_s
+    styles = Dir[style_root].map {|p| f = File.basename(p); f[1..f.length - (ext.length + 1)] }
+
+    styles.push("image", "markup")
   end
 
   def self.default_slide
