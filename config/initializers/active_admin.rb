c3e13af40b168e0e2b855c9a015d7415403778b9
Ricky Chilcott <ricky@rakefire.io>
2018-06-22 10:51:44 -0400

    Add AA Footer and Remove comments

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -119,7 +119,7 @@ ActiveAdmin.setup do |config|
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.comments = false
+  config.comments = false
   #
   # You can change the name under which comments are registered:
   # config.comments_registration_name = 'AdminComment'
@@ -282,12 +282,5 @@ ActiveAdmin.setup do |config|
   # By default, the footer shows the current Active Admin version. You can
   # override the content of the footer here.
   #
-  # config.footer = 'my custom footer text'
-
-  # == Sorting
-  #
-  # By default ActiveAdmin::OrderClause is used for sorting logic
-  # You can inherit it with own class and inject it for all resources
-  #
-  # config.order_clause = MyOrderClause
+  config.footer = 'Developed by Ricky Chilcott. ricky@rickychilcott.com 440-223-5715'
 end
