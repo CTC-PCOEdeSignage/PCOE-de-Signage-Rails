d24f3c466f6e7b9da04a833a861e48c22f719007
Ricky Chilcott <ricky@rakefire.io>
2018-06-22 10:52:14 -0400

    Require Room Name

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  validates_presence_of :building, :room, :libcal_identifier
+  validates_presence_of :name, :building, :room, :libcal_identifier
   validates_numericality_of :libcal_identifier
 
   has_many :room_screens, -> { order(position: :asc) }
