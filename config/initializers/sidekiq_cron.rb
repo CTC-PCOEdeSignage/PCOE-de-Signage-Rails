f015dcaf4390a186ae4f2174fc95803368aed123
Ricky Chilcott <ricky@rakefire.io>
2020-06-26 08:47:53 -0400

    Schedule sidekiq cron jobs in all environments to fix CI

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -1,6 +1,4 @@
 require Rails.root.join("app", "jobs", "event_finisher_job")
 
 schedule_file = "config/schedule.yml"
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-end
+Sidekiq::Cron::Job.load_from_hash(YAML.load_file(schedule_file)) if File.exist?(schedule_file)
--- spec/misc/event_finisher_scheduled_spec.rb
+++ spec/misc/event_finisher_scheduled_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.describe "event_finisher cron job" do
   # If any of these testse are failing, you will need to manually
   # remove all cron jobs (using Sidekiq cron web interface) and
-  # then restart the rails server. Lastly, re-run the test suite
-  let(:job) { Sidekiq::Cron::Job.find "event_finisher" }
+  # then re-run the test suite.
+  subject { Sidekiq::Cron::Job.find "event_finisher" }
 
   it "should be scheduled" do
-    expect(job).to be_present
+    expect(subject).to be_present
   end
 
   it "should happen ever hour; 5 after the hour" do
-    expect(job.cron).to eq("5 * * * *")
+    expect(subject.cron).to eq("5 * * * *")
   end
 end
