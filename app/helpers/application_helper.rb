13d1ea1724eb96b8654e425dee5e54c02c7aa146
Ricky Chilcott <ricky@rakefire.io>
2020-06-26 17:00:15 -0400

    Add some image branding.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def ohio_image(color: :white)
+    case color.to_sym
+    when :white
+      image_tag("OHIO_Patton_Logo_White.png", alt: "Ohio University", class: "img-fluid", style: "height: 100px;")
+    when :black
+      image_tag("OHIO_Patton_Logo_Black.gif", alt: "Ohio University", class: "img-fluid", style: "height: 100px;")
+    else
+    end
+  end
+
+  def patton_college_image
+    image_tag("CalledToLead-Blue.gif", alt: "Patton College of Education", style: "height: 100px;")
+  end
 end
