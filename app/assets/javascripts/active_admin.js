442b89ddebbaaf2054b349cd64fe07b1f4cb4f8b
Ricky Chilcott <ricky@rakefire.io>
2020-08-14 08:57:07 -0400

    only try to attach ace editor if it exists on the page

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,12 +2,13 @@
 
 document.addEventListener('DOMContentLoaded', function (event) {
   var editorElement = document.querySelector("div#editor")
-  var editor = ace.edit("editor")
-  var mirroredElement = document.querySelector(editorElement.dataset.mirrorWith)
+  if (editorElement) {
+    var editor = ace.edit("editor")
+    var mirroredElement = document.querySelector(editorElement.dataset.mirrorWith)
 
-  editor.session.setMode("ace/mode/yaml")
-  editor.getSession().on('change', function () {
-    mirroredElement.value = editor.getSession().getValue();
-  })
+    editor.session.setMode("ace/mode/yaml")
+    editor.getSession().on('change', function () {
+      mirroredElement.value = editor.getSession().getValue();
+    })
+  }
 })
-
