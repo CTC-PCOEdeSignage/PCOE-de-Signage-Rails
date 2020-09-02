3d8907cf705f9e4f99342b29ae9875a21a07d266
Ricky Chilcott <ricky@rakefire.io>
2020-09-02 11:31:02 -0400

    Improve SMTP settings to support Office 365

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -76,9 +76,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
@@ -107,11 +107,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => ENV["SMTP_SERVER"] || "smtp.Office365.com",
-    :port => ENV["SMTP_PORT"] || 587,
-    :user_name => ENV["SMTP_USERNAME"] || "coe-projrms-sa@ohio.edu",
-    :authentication => "plain",
-    :enable_starttls_auto => true,
+    address: ENV["SMTP_SERVER"] || "smtp.office365.com",
+    port: (ENV["SMTP_PORT"] || 587).to_i,
+    user_name: ENV["SMTP_USERNAME"] || "coe-projrms-sa@ohio.edu",
+    password: ENV["SMTP_PASSWORD"] || "",
+    authentication: :login,
+    domain: "ohio.edu",
+    enable_starttls_auto: true,
   }
   config.action_mailer.default_url_options = { host: ENV["HOST"] || "webserver.coe.ohio.edu" }
   Rails.application.routes.default_url_options = config.action_mailer.default_url_options
