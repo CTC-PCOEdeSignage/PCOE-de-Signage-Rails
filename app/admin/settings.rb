c5a4187a353d59d5950249583f8453c6b2019d7c
Ricky Chilcott <ricky@rakefire.io>
2021-08-24 13:43:32 -0400

    Create the settings folder if needed

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,4 +1,5 @@
 require "dry/schema"
+require "fileutils"
 
 ActiveAdmin.register_page "Settings" do
   class InvalidSchema < StandardError; end
@@ -10,6 +11,7 @@ ActiveAdmin.register_page "Settings" do
     schema = Config.schema.call(settings.try(:deep_symbolize_keys))
     raise InvalidSchema, schema.errors(full: true).to_a.map(&:to_s).to_sentence unless schema.errors.empty?
 
+    FileUtils.mkdir_p(Rails.root.join("config/settings"))
     File.write(Rails.root.join("config/settings/#{Rails.env}.yml"), @raw_yaml)
     Config.reload!
     redirect_to admin_settings_path, notice: "Settings Updated"
