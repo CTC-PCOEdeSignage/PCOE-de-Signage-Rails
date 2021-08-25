12b42febccd328123305e7d3c2388048e2041dfb
Ricky Chilcott <ricky@rakefire.io>
2021-08-25 08:12:11 -0400

    Include am/pm in user-facing time formats

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -3,3 +3,6 @@ Date::DATE_FORMATS[:day] = "%A"
 
 Time::DATE_FORMATS[:hour_min_ampm] = "%l:%M %p"
 Time::DATE_FORMATS[:full_datetime] = "%D %l:%M %p"
+
+Time::DATE_FORMATS[:long] = "%B %d, %Y %H:%M %p"
+Time::DATE_FORMATS[:long] = "%B %d, %Y %H:%M %p"
