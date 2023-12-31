5b42e2295887ef97fab341ee89c2c2196bd56299
Ricky Chilcott <ricky@missionmet.com>
2023-12-31 12:42:53 -0500

    Upgrade gems

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.ransackable_attributes(auth_object = nil)
+    column_names
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    reflect_on_all_associations.map(&:name).map(&:to_s)
+  end
 end
